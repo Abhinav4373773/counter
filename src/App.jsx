9bdc838af823d7f782937dabaa38b28b41990d84
Abhinav4373773 <hsin41349@gmail.com>
2026-03-25 00:47:37 +0530

    Direct commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,55 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Car } from './comnents/Car'
+import Mychild from './comnents/Mychild'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, pila] = useState(0)
+
+  // useEffect(()=>{
+  //   setTimeout(()=>{setCount(prev=>prev+1)},5000)
+  // })
 
   function handleIncreament ()
   {
     if (count<10)
     {
-      setCount((prev)=>prev+1)
+      pila(count+1)
+      console.log(count)
+      console.log(count)
+      console.log(count)
+      console.log(count)
+      console.log(count)
+      console.log(count)
     }
-    else
-    {
-      setCount(10)
-      alert("Count value cannot be greater than 10")
-    }
-    console.log(count)
+    // else
+    // {
+    //   pila(10)
+    //   alert("Count value cannot be greater than 10")
+    // }
+    console.log("Increment",count)
   }
   function handleDecreament(){
     if (count>0){
-      setCount((prev)=>prev-1)
+      pila((prev)=>prev-1)
+      pila((prev)=>prev-1)
+      console.log(count);
     }
     else if (count==0){
-      setCount(0)
+      pila(0)
       alert("Count Value Cannot be less than zero")
     }
     console.log(count)
   }
-  console.log(count)
   return (
     <>
-      <p style={backgroundclolor=red}>Count: {count}</p>
-      <button onClick={handleIncreament}>Incrase</button>
+      <Car name="Harsh" game="Abhinav">My name is Abhinmav Singh</Car>
+      <p>Count: {count}</p>
+      <button disabled={false} onClick={handleIncreament}>Incrase</button>
       <button onClick={handleDecreament}>Decrease</button>
+      <Mychild/>
     </>
   )
 }
---
+++ src/comnents/Box1.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Box1 = () => {
+  return (
+    <div>Box1</div>
+  )
+}
